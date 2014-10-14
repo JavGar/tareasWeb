9d6f5a3798cf0f99549fa50d7e4ac990ca4023c3
JavGar <dushtajavier@hotmail.com>
2014-10-14 10:16:27 -0500

    Añade carpeta de la nueva pagina y el iniio del formulario de regitro

---
+++ pagina2/registro.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Registro</title>
+</head>
+<body>
+	<form>
+		<label for="usuario">Usuario: </label>
+		<input type="email" placeholder="email">
+	</form>
+</body>
+</html>
