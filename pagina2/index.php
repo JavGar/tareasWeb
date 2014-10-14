2945b344a0552ecff8ec046cb8744ffa33fc0ce6
JavGar <dushtajavier@hotmail.com>
2014-10-14 10:56:16 -0500

    Añade un index con algo de formato

---
+++ pagina2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<meta charset="UTF-8">
+	<title>Index</title>
+</head>
+<body>
+	<header class="jumbotron">
+		<h1>Bienvenido</h1>
+	</header>
+
+	<div class="jumbotron">
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Regitrarse</h2>
+			</div>
+			<div class="col-md-6">
+				<h2>Entrar</h2>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
