e5018b0681ad2e8915b2a8e118ca096cbed3b635
JavGar <dushtajavier@hotmail.com>
2014-10-14 10:21:56 -0500

    Añade mas campos al formulario de registro

--- pagina2/registro.php
+++ pagina2/registro.php
@@ -7,6 +7,12 @@
 <body>
 	<form>
 		<label for="usuario">Usuario: </label>
+		<input type="text" placeholder="usuario">
+		<br>
+		<label for="contra">Contraseña: </label>
+		<input type="password" placeholder="contraseña">
+		<br>
+		<label for="correo">Email: </label>
 		<input type="email" placeholder="email">
 	</form>
 </body>
